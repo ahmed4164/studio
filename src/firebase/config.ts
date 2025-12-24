0ba8f2537f2d90e71ddc245984bf0a602568e546
Ahmed Mohammed <ahmed@albearoti.com>
2025-12-24 06:53:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6814194924-d64b6",
+  "appId": "1:1072681880322:web:8ab15d6482174dd4be501b",
+  "apiKey": "AIzaSyCx3CXQc99TpyyyRSd8VRE4iX6r_lI_k64",
+  "authDomain": "studio-6814194924-d64b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1072681880322"
+};
